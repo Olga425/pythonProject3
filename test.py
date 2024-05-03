5d60de8dc24576160be97dae31a37e442af0247a
Olga425 <olga.r.it@gmail.com>
2024-05-03 11:06:09 +0300

    Calculator 24.2.3

---
+++ test.py
@@ -0,0 +1,25 @@
+
+import pytest
+
+from app.calc import Calculator
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_adding_success(self):
+        assert self.calc.adding(self, 3, 3) == 6
+
+    def test_multiply_success(self):
+        assert self.calc.multiply(self, 3, 3) == 9
+
+    def test_division_success(self):
+        assert self.calc.division(self, 3, 1) == 3
+
+    def test_subtraction_success(self):
+        assert self.calc.subtraction(self, 6, 3) == 3
+
+    def teardown(self):
+        print('Выполнение метода Teardown')
+
+
